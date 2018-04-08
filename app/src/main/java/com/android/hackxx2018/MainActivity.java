f23b51b6d280e4e0c2b99cee1b65a8e8af4b2c9b
Catherine <catherinesun31@gmail.com>
2018-04-08 06:28:10 -0700

    Friends List appears when user opens app

--- app/src/main/java/com/android/hackxx2018/MainActivity.java
+++ app/src/main/java/com/android/hackxx2018/MainActivity.java
@@ -1,16 +1,16 @@
 package com.android.hackxx2018;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
-    FriendsList friendsList = new FriendsList();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,7 +18,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        friendsList.addFriend("Catherine");
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -27,6 +26,9 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+        Intent toFriendsListIntent = new Intent(this, FriendsList.class);
+
+        startActivity(toFriendsListIntent);
     }
 
     @Override
