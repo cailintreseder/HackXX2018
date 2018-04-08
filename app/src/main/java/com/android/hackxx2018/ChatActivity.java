4d124922cceab2bf20cd987a97dae11cc07213c2
Catherine <catherinesun31@gmail.com>
2018-04-08 15:59:40 -0700

    merge

--- app/src/main/java/com/android/hackxx2018/ChatActivity.java
+++ app/src/main/java/com/android/hackxx2018/ChatActivity.java
@@ -26,8 +26,8 @@ import java.util.concurrent.ExecutionException;
 
 public class ChatActivity extends AppCompatActivity {
     private FirebaseListAdapter<ChatMessage> adapter;
-    String firstLang = "";
-    String secondLang = "";
+    String firstLang = "en";
+    String secondLang = "zh";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -68,6 +68,7 @@ public class ChatActivity extends AppCompatActivity {
         //String translationResult = translatorBackgroundTask.execute(textToBeTranslated,languagePair); // Returns the translated text as a String
         //String translationResult = translatorBackgroundTask.execute(textToBeTranslated,languagePair).get(); // Returns the translated text as a String
         String translationResult = null; // Returns the translated text as a String
+        //to do: get the language symbols
         try {
             translationResult = translatorBackgroundTask.execute(textToBeTranslated,languagePair).get();
         } catch (InterruptedException e) {
